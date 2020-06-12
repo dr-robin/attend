44046f17a281a2071e846a2f29eae4ab7f34c97c
Robin <robinmcgregor@gmail.com>
2020-06-12 10:15:28 +0200

    Update attend.py

--- attend.py
+++ attend.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-
 import subprocess
 import pyautogui
 import webbrowser
 
-#Open discord
-subprocess.call(["discord"])
-
-#Maximise current window
-pyautogui.press(["alt", "F10"])
-
-#switch discord server
-pyautogui.press(["ctr", "2"])
-print("don't forget to join the general voice channel")
-
 #Open my.becode
 webbrowser.open('https://my.becode.org/dashboard')
-pyautogui.press(["F5"])
-print("don't forget to click attendence time")
+pyautogui.press(["F5"])  #refresh webpage
+pyautogui.press(["alt", "F10"]) #maximise screen
+pyautogui.press(["ctr", "f"])  #find time
+pyautogui.write("09:00")
+pyautogui.press(["enter"])
+pyautogui.click(314, 498, clicks=1,duration=10)  #mouseclick
+
+#Open discord
+subprocess.call("/usr/bin/discord")
+pyautogui.press(["alt", "F10"]) #Maximise current window
+pyautogui.press(["ctr", "2"])  #switch discord server
+
+#Make pop-up reminder window
+import tkinter
+window = tkinter.Tk()
+# to rename the title of the window
+window.title("WARNING ATTENDANCE CHECK")
+# pack is used to show the object in the window
+label = tkinter.Label(window, text = "Don't forget to check your attendance at my.becode.org").pack()
+window.mainloop()
